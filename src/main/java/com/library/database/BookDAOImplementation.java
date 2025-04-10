32a724d978b57625700824ff17fc924ea4f18880
Arman Bhatia <arman.bhatia.1407@gmail.com>
2025-04-10 19:53:25 -0400

    fetching books from database done; wokring on table uifix

--- src/main/java/com/library/database/BookDAOImplementation.java
+++ src/main/java/com/library/database/BookDAOImplementation.java
@@ -1,10 +1,10 @@
 package com.library.database;
 
 import com.library.model.Book;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -83,7 +83,6 @@ public class BookDAOImplementation {
                 String added_date = resultRows.getString("added_date");
 
                 System.out.printf("%-30s %-30s %-30s %-30s %-30s\n", name, auth, isbn, status, added_date);
-
             }
 
         } catch (SQLException e) {
@@ -91,29 +90,50 @@ public class BookDAOImplementation {
         }
     }
 
-    public static void getAllBooks() {
+    public static ArrayList<ArrayList<String>> getAllBooks() {
+
         String query = "SELECT * FROM books";
 
+
+        ArrayList<ArrayList<String>> books = new ArrayList<>();
+
         try {
+
             PreparedStatement pstmt = con.prepareStatement(query);
             ResultSet resultRows = pstmt.executeQuery();
 
-            System.out.printf("%-30s %-30s %-30s %-30s %-30s\n", "name", "auth", "isbn", "status", "added_date");
-            System.out.printf("%-150s\n", "-----------------------------------------------------------------------------------------------------------------------------------");
 
             while (resultRows.next()) {
-                String name = resultRows.getString("title");
+                ArrayList<String> book = new ArrayList<>();
+                String title = resultRows.getString("title");
                 String auth = resultRows.getString("author");
                 String isbn = resultRows.getString("isbn");
                 String status = resultRows.getString("status");
                 String added_date = resultRows.getString("added_date");
 
-                System.out.printf("%-30s %-30s %-30s %-30s %-30s\n", name, auth, isbn, status, added_date);
+
+                // Adding a single book DATA to book ArrayList {name,auth,isbn,etc}
+                book.add(title);
+                book.add(auth);
+                book.add(isbn);
+                book.add(status);
+                book.add(added_date);
+
+                // Adding a BOOK to the array of BOOKS
+                books.add(book);
+
+                //Clearing the array of book to store next book values
+                book.clear();
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return books;
+    }
+
+    public static void main(String[] args) {
+        getAllBooks();
     }
 
     public static void updateStatus(int BookID, Book.Status status) {
